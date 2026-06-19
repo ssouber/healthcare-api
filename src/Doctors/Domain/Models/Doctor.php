47e537b2b13ec21c65c3392ba29c5407765e8a86
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-19 17:08:45 -0300

    feat: create Doctor migration and model

---
+++ src/Doctors/Domain/Models/Doctor.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Doctors\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int                          $id
+ * @property string                       $name
+ * @property \Carbon\CarbonImmutable|null $created_at
+ * @property \Carbon\CarbonImmutable|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Doctor whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Doctor extends Model
+{
+    protected $guarded = ['id'];
+}
