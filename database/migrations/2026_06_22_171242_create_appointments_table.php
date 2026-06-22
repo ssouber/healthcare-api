ebaed1bd58111b679a28d2a2d55c236737424968
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-22 15:41:48 -0300

    feat(migrations): use Laravel's softDeletes()

--- database/migrations/2026_06_22_171242_create_appointments_table.php
+++ database/migrations/2026_06_22_171242_create_appointments_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('status')->default('scheduled'); // 'scheduled' | 'cancelled'
             $table->timestamp('starts_at');
             $table->timestamp('ends_at');
-            $table->timestamp('deleted_at'); // soft delete
+            $table->softDeletes();
             $table->timestamp('created_at');
             $table->timestamp('updated_at');
         });
