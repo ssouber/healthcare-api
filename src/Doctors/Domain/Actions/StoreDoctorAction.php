bec9cd49beaa8e6c8a7d2b8a9787c83855d676a1
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-24 16:01:59 -0300

    fix(action): wrap up doctor save and sync inside a DB::transaction

--- src/Doctors/Domain/Actions/StoreDoctorAction.php
+++ src/Doctors/Domain/Actions/StoreDoctorAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lightit\Doctors\Domain\Actions;
 
+use Illuminate\Support\Facades\DB;
 use Lightit\Doctors\Domain\DataTransferObjects\DoctorDto;
 use Lightit\Doctors\Domain\Models\Doctor;
 
@@ -11,14 +12,17 @@ class StoreDoctorAction
 {
     public function execute(DoctorDto $doctorDto): Doctor
     {
-        $doctor = new Doctor();
-        $doctor->name = $doctorDto->name;
-        $doctor->saveOrFail();
+        return DB::transaction(function () use ($doctorDto) {
+            $doctor = new Doctor();
+            $doctor->name = $doctorDto->name;
 
-        if ($doctorDto->clinics !== null) {
-            $doctor->clinics()->syncOrFail($doctorDto->clinics);
-        }
+            $doctor->saveOrFail();
 
-        return $doctor;
+            if ($doctorDto->clinics !== null) {
+                $doctor->clinics()->syncOrFail($doctorDto->clinics);
+            }
+
+            return $doctor->load('clinics');
+        });
     }
 }
