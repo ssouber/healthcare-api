6d43d47bb544f06906a86412c1b5df4fe9a6227d
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-24 12:27:09 -0300

    feat(factory): add doctor factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use Database\Factories\DoctorFactory;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,5 +19,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         UserFactory::new()->createMany(35);
+        DoctorFactory::new()->createMany(35);
     }
 }
