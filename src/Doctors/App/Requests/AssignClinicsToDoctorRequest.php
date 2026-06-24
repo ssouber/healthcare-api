44c737c61897549f6736d05195196034a63bcf86
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-24 17:44:29 -0300

    fix(request): prevent N+1

--- src/Doctors/App/Requests/AssignClinicsToDoctorRequest.php
+++ src/Doctors/App/Requests/AssignClinicsToDoctorRequest.php
@@ -15,8 +15,8 @@ class AssignClinicsToDoctorRequest extends FormRequest
     public function rules(): array
     {
         return [
-            self::CLINICS => ['sometimes', 'array'],
-            self::CLINICS . '.*' => [Rule::numeric()->integer(), Rule::exists(Clinic::class, 'id')],
+            self::CLINICS => ['sometimes', 'array', Rule::exists(Clinic::class, 'id')],
+            self::CLINICS . '.*' => [Rule::numeric()->integer()],
         ];
     }
 
--- src/Doctors/App/Requests/StoreDoctorRequest.php
+++ src/Doctors/App/Requests/StoreDoctorRequest.php
@@ -19,8 +19,8 @@ class StoreDoctorRequest extends FormRequest
     {
         return [
             self::NAME => ['required', 'string', 'min:4', 'max:255'],
-            self::CLINICS => ['sometimes', 'array'],
-            self::CLINICS . '.*' => [Rule::numeric()->integer(), Rule::exists(Clinic::class, 'id')],
+            self::CLINICS => ['sometimes', 'array', Rule::exists(Clinic::class, 'id')],
+            self::CLINICS . '.*' => [Rule::numeric()->integer()],
         ];
     }
 
