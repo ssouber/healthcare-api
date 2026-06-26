d0fab0a3229044c2098d958e3bb254cb29524411
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-26 17:24:03 -0300

    fix(controller): add missing uses

--- src/Appointments/App/Controllers/StoreAppointmentController.php
+++ src/Appointments/App/Controllers/StoreAppointmentController.php
@@ -7,7 +7,9 @@ namespace Lightit\Appointments\App\Controllers;
 use Dedoc\Scramble\Attributes\Endpoint;
 use Dedoc\Scramble\Attributes\Group;
 use Illuminate\Http\JsonResponse;
+use Lightit\Appointments\App\Requests\StoreAppointmentRequest;
 use Lightit\Appointments\App\Resources\AppointmentResource;
+use Lightit\Appointments\Domain\Actions\StoreAppointmentAction;
 
 #[Group('Appointments')]
 final readonly class StoreAppointmentController
