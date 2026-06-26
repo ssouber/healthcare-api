06dd2e0a02494e9f778773b3891c1bfa55ea4f1a
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-26 17:19:15 -0300

    fix(action): return $appointment with relationshop loaded was missing

--- src/Appointments/Domain/Actions/StoreAppointmentAction.php
+++ src/Appointments/Domain/Actions/StoreAppointmentAction.php
@@ -19,21 +19,25 @@ class StoreAppointmentAction
      */
     public function execute(AppointmentDto $dto): Appointment
     {
-        if ($this->hasOverlap('doctor_id', $dto->doctorId, $dto->startsAt)) {
+        $startsAt = CarbonImmutable::parse($dto->startsAt);
+
+        if ($this->hasOverlap('doctor_id', $dto->doctorId, $startsAt)) {
             throw new DoctorNotAvailableException();
         }
 
-        if ($this->hasOverlap('patient_id', $dto->patientId, $dto->startsAt)) {
+        if ($this->hasOverlap('patient_id', $dto->patientId, $startsAt)) {
             throw new PatientNotAvailableException();
         }
 
         $appointment = new Appointment();
         $appointment->doctor_id = $dto->doctorId;
         $appointment->patient_id = $dto->patientId;
-        $appointment->starts_at = $dto->startsAt;
-        $appointment->ends_at = $appointment->starts_at->addHour();
+        $appointment->starts_at = $startsAt;
+        $appointment->ends_at = $startsAt->addHour();
 
         $appointment->saveOrFail();
+
+        return $appointment->load('doctor', 'patient', 'clinic');
     }
 
     private function hasOverlap(string $column, int $id, CarbonImmutable $startsAt): bool
