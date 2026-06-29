f59432e9b6a39bc3c65abbf406446f5727299763
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-06-29 12:47:08 -0300

    fix(appointments): use DoctorResource::make() instead of new DoctorResource to follow project convention

--- src/Appointments/App/Resources/AppointmentResource.php
+++ src/Appointments/App/Resources/AppointmentResource.php
@@ -22,9 +22,9 @@ class AppointmentResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'doctor' => new DoctorResource($this->whenLoaded('doctor')),
-            'patient' => new PatientResource($this->whenLoaded('patient')),
-            'clinic' => new ClinicResource($this->whenLoaded('clinic')),
+            'doctor' => DoctorResource::make()->whenLoaded('doctor'),
+            'patient' => PatientResource::make()->whenLoaded('patient'),
+            'clinic' => ClinicResource::make()->whenLoaded('clinic'),
             'starts_at' => $this->starts_at,
             'ends_at' => $this->ends_at,
         ];
