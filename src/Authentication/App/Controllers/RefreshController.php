3d635a1104834d8b0040c7072b781d8b8bcd9323
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-07-01 10:35:13 -0300

    docs(auth): add Endpoint attribute to RefreshController

--- src/Authentication/App/Controllers/RefreshController.php
+++ src/Authentication/App/Controllers/RefreshController.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Lightit\Authentication\App\Controllers;
 
+use Dedoc\Scramble\Attributes\Endpoint;
 use Illuminate\Http\JsonResponse;
 use Lightit\Authentication\App\Resources\AuthTokenResource;
 use Lightit\Authentication\Domain\Actions\RefreshTokenAction;
 
 final class RefreshController
 {
+    #[Endpoint(
+        operationId: 'refreshToken',
+        title: 'Refresh Token',
+        description: 'Refresh logged patient token.',
+    )]
     public function __invoke(RefreshTokenAction $action): JsonResponse
     {
         $tokenData = $action->execute();
