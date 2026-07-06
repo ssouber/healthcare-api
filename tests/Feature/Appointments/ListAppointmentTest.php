75ca3601dceb0e9d73055d63dffc3b16d9d2fc57
Sebastian Souberbielle <sebastian.souberbielle@lightit.io>
2026-07-06 11:51:28 -0300

    test(appointments): list appointments

--- tests/Feature/Appointments/ListAppointmentTest.php
+++ tests/Feature/Appointments/ListAppointmentTest.php
@@ -2,5 +2,60 @@
 
 declare(strict_types=1);
 
-it('', function (): void {
+use Database\Factories\AppointmentFactory;
+use Database\Factories\PatientFactory;
+use Lightit\Appointments\App\Resources\AppointmentResource;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\getJson;
+
+describe('appointments', function (): void {
+    /** @see ListAppointmentController */
+    it('lists the authenticated patient appointments', function (): void {
+        $patient = PatientFactory::new()->createOne();
+
+        $appointments = AppointmentFactory::new()
+            ->forPatient($patient)
+            ->createMany(3)
+            ->load('clinic', 'doctor', 'patient')
+            ->sortByDesc('starts_at');
+
+        /** @var array{data: array} $expectedResponse */
+        $expectedResponse = AppointmentResource::collection($appointments)
+            ->response()
+            ->getData(true);
+
+        actingAs($patient)
+            ->getJson(url('api/appointments'))
+            ->assertSuccessful()
+            ->assertJsonPath('data', $expectedResponse['data']);
+    });
+
+    it('returns and empty array', function (): void {
+        $patient = PatientFactory::new()->createOne();
+
+        actingAs($patient)
+            ->getJson(url('api/appointments'))
+            ->assertSuccessful()
+            ->assertJsonCount(0, 'data');
+    });
+
+    it('returns unauthorized error when patient is no authenticated', function (): void {
+        AppointmentFactory::new()
+            ->createMany(3)
+            ->load('clinic', 'doctor', 'patient')
+            ->sortByDesc('starts_at');
+
+        getJson(url('api/appointments'))
+            ->assertUnauthorized();
+    });
+
+    it('return unauthorized error when patient is no authenticated', function (): void {
+        AppointmentFactory::new()
+            ->createMany(3)
+            ->load('clinic', 'doctor', 'patient')
+            ->sortByDesc('starts_at');
+
+        getJson(url('api/appointments'))
+            ->assertUnauthorized();
+    });
 });
